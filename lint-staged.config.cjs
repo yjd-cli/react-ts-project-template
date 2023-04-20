1fc2ea65885c88a9daee06b7e27c17108bd246b2
yjd <yaojiadong@yjd666deMacBook-Pro.local>
2023-04-20 21:57:21 +0800

    feat: test7

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   "src/**/*.{js,jsx,ts,tsx}": [
+    // https://eslint.org/docs/latest/use/command-line-interface#options
    "eslint --fix",
+   // https://prettier.io/docs/en/cli.html
    "prettier --write"
   ],
   "src/**/*.{css,less}": [
     // https://stylelint.io/user-guide/cli#options
-    "stylelint --fix",
-    "eslint --fix",
+    "stylelint --fix --formatter verbose",
     "prettier --write"
    ],
 };
