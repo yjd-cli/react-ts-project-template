91bc8be9c3cb25172def4b44d6447aad3012e16b
yjd <yaojiadong@yjd666deMacBook-Pro.local>
2023-04-20 21:52:45 +0800

    feat: test6

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,13 +1,12 @@
 module.exports = {
   "src/**/*.{js,jsx,ts,tsx}": [
    "eslint --fix",
-   "prettier --write",
-   //  "git add"
+   "prettier --write"
   ],
   "src/**/*.{css,less}": [
     // https://stylelint.io/user-guide/cli#options
     "stylelint --fix",
-    "prettier --write",
-    // "git add"
+    "eslint --fix",
+    "prettier --write"
    ],
 };
